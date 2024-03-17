97a92961e7aebca8a415bc5c3ef358ffe49744fa
Stoupy51 <stoupy51@gmail.com>
2024-03-17 17:08:47 +0100

    Refactor build process and add pack.mcmeta file

--- src/finalyze.py
+++ src/finalyze.py
@@ -14,15 +14,15 @@ datapack_path = f"{ROOT}/{BUILD_FOLDER}/{NAME}_datapack"
 resourcepack_path = f"{ROOT}/{BUILD_FOLDER}/{NAME}_resource_pack"
 if os.path.exists(BUILD_DATAPACK):
 	shutil.make_archive(datapack_path, 'zip', BUILD_DATAPACK)
-	info(f"Datapack zip file generated at '{datapack_path}.zip'")
+	info(f"Datapack zip file generated at '{datapack_path.replace(f'{ROOT}/','')}.zip'")
 else:
-	warning(f"Datapack folder not found at '{BUILD_DATAPACK}'")
+	warning(f"Datapack folder not found at '{BUILD_DATAPACK.replace(f'{ROOT}/','')}'")
 
 if os.path.exists(BUILD_RESOURCE_PACK):
 	shutil.make_archive(resourcepack_path, 'zip', BUILD_RESOURCE_PACK)
-	info(f"Resource pack zip file generated at '{resourcepack_path}.zip'")
+	info(f"Resource pack zip file generated at '{resourcepack_path.replace(f'{ROOT}/','')}.zip'")
 else:
-	warning(f"Resource pack folder not found at '{BUILD_RESOURCE_PACK}'")
+	warning(f"Resource pack folder not found at '{BUILD_RESOURCE_PACK.replace(f'{ROOT}/','')}'")
 
 
 # Copy datapack and resource pack to a specific location
@@ -30,20 +30,20 @@ if BUILD_COPY_DESTINATIONS and type(BUILD_COPY_DESTINATIONS) == tuple:
 	if os.path.exists(datapack_path + ".zip"):
 		if type(BUILD_COPY_DESTINATIONS[0]) == str:
 			shutil.copy(datapack_path + ".zip", BUILD_COPY_DESTINATIONS[0])
-			info(f"Datapack zip file copied to '{BUILD_COPY_DESTINATIONS[0]}'")
+			info(f"Datapack zip file copied to '{BUILD_COPY_DESTINATIONS[0]}/'")
 		else:
 			warning("First copy destination is not a string")
 	else:
-		warning(f"Datapack zip file not found at '{datapack_path}.zip'")
+		warning(f"Datapack zip file not found at '{datapack_path.replace(f'{ROOT}/','')}.zip'")
 
 	if os.path.exists(resourcepack_path + ".zip"):
 		if type(BUILD_COPY_DESTINATIONS[1]) == str:
 			shutil.copy(resourcepack_path + ".zip", BUILD_COPY_DESTINATIONS[1])
-			info(f"Resource pack zip file copied to '{BUILD_COPY_DESTINATIONS[1]}'")
+			info(f"Resource pack zip file copied to '{BUILD_COPY_DESTINATIONS[1]}/'")
 		else:
 			warning("Second copy destination is not a string")
 	else:
-		warning(f"Resource pack zip file not found at '{resourcepack_path}.zip'")
+		warning(f"Resource pack zip file not found at '{resourcepack_path.replace(f'{ROOT}/','')}.zip'")
 
 
 # Remove __pycache__ folders because they are annoying
---
+++ src/initialize.py
@@ -0,0 +1,18 @@
+
+# Import config
+from src.config import *
+
+# Delete build folder
+print()
+shutil.rmtree(BUILD_FOLDER, ignore_errors=True)
+
+
+# Setup pack.mcmeta
+with super_open(f"{BUILD_DATAPACK}/pack.mcmeta", "w") as file:
+	pack_mcmeta =  {"pack":{"pack_format": PACK_FORMAT, "description": DESCRIPTION}, "id": NAMESPACE}
+	json.dump(pack_mcmeta, file, indent = '\t')
+	file.write("\n")
+	info(f"pack.mcmeta file created")
+	pass
+
+

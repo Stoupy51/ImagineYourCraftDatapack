fd4678d4339c532a2c436140dc3873a6d41e20b7
Stoupy51 <stoupy51@gmail.com>
2024-03-18 08:39:03 +0100

    Add color constants and utility functions

--- src/config.py
+++ src/config.py
@@ -11,20 +11,6 @@ import os
 # Import time constant
 IMPORT_TIME = datetime.datetime.now()
 
-# Colors constants
-GREEN = "\033[92m"
-YELLOW = "\033[93m"
-RED = "\033[91m"
-RESET = "\033[0m"
-def current_time() -> str:
-	return datetime.datetime.now().strftime("%H:%M:%S")
-def info(text: str) -> None:
-	print(f"{GREEN}[INFO {current_time()}] {text}{RESET}")
-def warning(text: str) -> None:
-	print(f"{YELLOW}[WARNING {current_time()}] {text}{RESET}")
-def error(text: str) -> None:
-	print(f"{RED}[ERROR {current_time()}] {text}{RESET}")
-
 # Datapack related constants
 MINECRAFT_VERSION = "24w11a" # 1.20.5 snapshot
 VERSION = "0.0.1"

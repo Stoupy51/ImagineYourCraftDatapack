1e1f580783f5e021b764633f25cb35f51ef1e7e0
Stoupy51 <stoupy51@gmail.com>
2024-03-20 22:14:18 +0100

    Debugging #2

--- src/resource_pack/vanilla_models.py
+++ src/resource_pack/vanilla_models.py
@@ -7,16 +7,35 @@ vanilla_ids = []
 for v in DATABASE.values():
 	if v["id"] not in vanilla_ids:
 		vanilla_ids.append(v["id"].replace("minecraft:", ""))
+	pass
+
+"""{
+    "parent": "item/handheld",
+  	"textures": {
+    	"layer0": "item/leather_chestplate",
+    	"layer1": "item/leather_chestplate_overlay"
+  	},
+"""
 
 # For each vanilla ID, create the json model file
-blocks = [CUSTOM_BLOCK_VANILLA]
+blocks = [CUSTOM_BLOCK_VANILLA, CUSTOM_ITEM_VANILLA]
 blocks = [x.replace("minecraft:", "") for x in blocks]
 for id in vanilla_ids:
 	with super_open(f"{BUILD_RESOURCE_PACK}/assets/minecraft/models/item/{id}.json", "w") as file:
 		block_or_item = "block" if id in blocks else "item"
-		content = {"parent": f"{block_or_item}/{id}", "overrides": []}
+		if id == CUSTOM_ITEM_VANILLA.replace("minecraft:", ""):
+			content = {"parent": f"block/{id}"}
+			block_or_item = "item"
+		else:
+			content = {"parent": f"{block_or_item}/{id}"}
+		if id not in blocks:
+			content["parent"] = "item/handheld"
+			content["textures"] = {"layer0": f"item/{id}"}
+			if id in ["leather_helmet", "leather_chestplate", "leather_leggings", "leather_boots"]:
+				content["textures"]["layer1"] = f"item/{id}_overlay"
 
 		# Get overrides
+		content["overrides"] = []
 		for item, data in DATABASE.items():
 			if data["id"].replace("minecraft:","") == id:
 				content["overrides"].append({"predicate": { "custom_model_data": data["custom_model_data"]}, "model": f"{NAMESPACE}:{block_or_item}/{item}" })

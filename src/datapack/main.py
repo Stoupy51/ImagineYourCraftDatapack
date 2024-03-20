04adf8b6691474c5da7e8ddbb12f1ea1874f2393
Stoupy51 <stoupy51@gmail.com>
2024-03-20 15:39:55 +0100

    Added TODO things

--- src/datapack/main.py
+++ src/datapack/main.py
@@ -9,3 +9,8 @@ import src.datapack.loading
 # Generate items loot tables
 import src.datapack.loot_tables
 
+# TODO: Basic structure (tick, second, minute)
+# TODO: Custom Blocks (place + destroy)
+# TODO: Custom Ores generation
+# TODO: Finally, for each .mcfunction file, add small header (called by what functions, with which context)
+

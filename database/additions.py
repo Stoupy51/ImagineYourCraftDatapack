c4d0f7a7d85f522e1f33fe3d2a04c324052158c4
Stoupy51 <stoupy51@gmail.com>
2024-03-23 12:26:23 +0100

    Items renaming

--- database/additions.py
+++ database/additions.py
@@ -14,6 +14,19 @@ database_additions = {
 		]
 	},
 	"iron_furnace": {"id": CUSTOM_BLOCK_VANILLA},
+
+	# Materials for food
+
+
+	# Foods
+	"apple_juice":		{"id": CUSTOM_ITEM_VANILLA, "food":{"can_always_eat":True, "nutrition":2, "saturation_modifier":0.6, "effects":[{"effect":"minecraft:absorption","duration":30*20,"amplifier":0,"probability":1.0}]}},
+	"beer":				{"id": CUSTOM_ITEM_VANILLA, "food":{"can_always_eat":True, "nutrition":2, "saturation_modifier":0.6, "effects":[{"effect":"minecraft:nausea","duration":30*20,"amplifier":0,"probability":1.0}]}},
+	"cherry":			{"id": CUSTOM_ITEM_VANILLA, "food":{"can_always_eat":True, "nutrition":2, "saturation_modifier":0.8}},
+	"cherry_cake":		{"id": CUSTOM_BLOCK_VANILLA},
+	"coca":				{"id": CUSTOM_ITEM_VANILLA, "food":{"can_always_eat":True, "nutrition":2, "saturation_modifier":0.6, "effects":[{"effect":"minecraft:strength","duration":30*20,"amplifier":0,"probability":1.0}]}},
+	"hot_chocolate":	{"id": CUSTOM_ITEM_VANILLA, "food":{"can_always_eat":True, "nutrition":3, "saturation_modifier":0.8, "effects":[{"effect":"minecraft:speed","duration":30*20,"amplifier":0,"probability":1.0}]}},
+	"nutella_toast":	{"id": CUSTOM_ITEM_VANILLA, "food":{"can_always_eat":True, "nutrition":4, "saturation_modifier":0.6, "effects":[{"effect":"minecraft:resistance","duration":30*20,"amplifier":0,"probability":1.0}]}},
+	"red_bull":			{"id": CUSTOM_ITEM_VANILLA, "food":{"can_always_eat":True, "nutrition":2, "saturation_modifier":0.6, "effects":[{"effect":"minecraft:regeneration","duration":30*20,"amplifier":0,"probability":1.0}]}},
 }
 
 # Update the database with new data

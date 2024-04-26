b97debcb51421b6ad7aee1aedf32d19f471397e2
Stoupy51 <stoupy51@gmail.com>
2024-04-26 15:32:11 +0200

    Loot table custom data now don't get from storage

--- src/datapack/loot_tables.py
+++ src/datapack/loot_tables.py
@@ -8,21 +8,14 @@ from src.utils.print import *
 for item, data in DATABASE.items():
 	loot_table = {"pools":[{"rolls":1,"entries":[{"type":"minecraft:item","name": data["id"]}]}]}
 
-	# Copy custom data from storage
-	copy_custom_data = {
-		"function":"minecraft:copy_custom_data",
-		"source":{"type":"minecraft:storage","source":f"{NAMESPACE}:items"},
-		"ops":[{"source":f"all.{item}.components.custom_data","target":"{}","op":"merge"}]
-	}
-
 	# Set components
 	set_components = {"function":"minecraft:set_components","components":{}}
 	for k, v in data.items():
-		if k not in NOT_COMPONENTS and k != "custom_data":
+		if k not in NOT_COMPONENTS:
 			set_components["components"][f"minecraft:{k}"] = v
 	
 	# Add functions
-	loot_table["pools"][0]["entries"][0]["functions"] = [copy_custom_data, set_components]
+	loot_table["pools"][0]["entries"][0]["functions"] = [set_components]
 
 	with super_open(f"{BUILD_DATAPACK}/data/{NAMESPACE}/loot_tables/i/{item}.json", "w") as f:
 		super_json_dump(loot_table, f, max_level = 9)
